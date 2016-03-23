94e52f29073dfa006b98f9b5dda40e08fbff5dd2
Patrick Burns <pburns@kineticsupply.com>
2016-03-23 12:01:46 -0500

    first commit

---
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('meanThree')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
